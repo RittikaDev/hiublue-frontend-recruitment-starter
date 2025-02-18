d0e497f93589c57ffb3b539f13b2982478ee79d9
Rittika Dev <rittika.dev@gmail.com>
2025-02-18 10:36:11 +0600

    feat: wrap layout with Provider for user authentication context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import * as React from 'react';
-import {AppRouterCacheProvider} from '@mui/material-nextjs/v15-appRouter';
-import CssBaseline from '@mui/material/CssBaseline';
-import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import * as React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import CssBaseline from "@mui/material/CssBaseline";
+import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
+
 import ThemeProvider from "@/theme/index";
+import Providers from "@/providers/Providers";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-        <body>
-        <InitColorSchemeScript attribute="class"/>
-        <AppRouterCacheProvider options={{enableCssLayer: true}}>
-            <ThemeProvider>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline/>
-                {props.children}
-            </ThemeProvider>
-        </AppRouterCacheProvider>
-        </body>
-        </html>
-    );
+	return (
+		<Providers>
+			<html lang="en" suppressHydrationWarning>
+				<body>
+					<InitColorSchemeScript attribute="class" />
+					<AppRouterCacheProvider options={{ enableCssLayer: true }}>
+						<ThemeProvider>
+							{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+							<CssBaseline />
+							{props.children}
+						</ThemeProvider>
+					</AppRouterCacheProvider>
+				</body>
+			</html>
+		</Providers>
+	);
 }
